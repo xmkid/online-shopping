5672c9f75fde998058c84ecb1e3483ab7e70537e
xmkid <xmkid@126.com>
2018-08-10 23:19:57 +0800

    spring setup

--- onlineshopping/src/main/java/net/niar/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/niar/onlineshopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package net.niar.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,4 +18,21 @@ public class PageController {
 		return mv;
 	}
 	
+	@RequestMapping(value="/test1")
+	public ModelAndView test1(@RequestParam("greeting")String greeting) {
+		ModelAndView mv = new ModelAndView("page");
+		
+		mv.addObject("greeting", greeting);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/test2/{greeting}-{name}")
+	public ModelAndView test2(@PathVariable("greeting")String greeting, @PathVariable("name")String name) {
+		ModelAndView mv = new ModelAndView("page");
+		
+		mv.addObject("greeting", greeting + ", " + name);
+		
+		return mv;
+	}
 }
